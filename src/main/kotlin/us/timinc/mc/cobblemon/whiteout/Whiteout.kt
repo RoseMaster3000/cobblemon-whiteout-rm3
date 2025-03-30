6675c991def157c7ba34671986f7f27dde748eb9
Shahrose Kasim <rosemaster3000@gmail.com>
2025-03-29 22:44:23 -0400

    Simualte PLayer kills when whiteout against opponent

--- src/main/kotlin/us/timinc/mc/cobblemon/whiteout/Whiteout.kt
+++ src/main/kotlin/us/timinc/mc/cobblemon/whiteout/Whiteout.kt
@@ -4,6 +4,8 @@ import com.cobblemon.mod.common.api.battles.model.actor.ActorType
 import com.cobblemon.mod.common.api.events.CobblemonEvents
 import com.cobblemon.mod.common.api.events.battles.BattleFaintedEvent
 import net.fabricmc.api.ModInitializer
+import net.minecraft.server.network.ServerPlayerEntity
+import org.apache.logging.log4j.core.jmx.Server
 
 object Whiteout : ModInitializer {
     const val MOD_ID = "cobblemon-whiteout"
@@ -13,12 +15,30 @@ object Whiteout : ModInitializer {
     }
 
     private fun handleBattleFainted(battleFaintedEvent: BattleFaintedEvent) {
-        val killed = battleFaintedEvent.killed
-        val entity = killed.entity ?: return
-        val owner = entity.owner ?: return
-        if (killed.actor.type != ActorType.PLAYER) return
-        if (killed.actor.pokemonList.all { it.health == 0 }) {
-            owner.kill()
+        // Fetch victim
+        val victimPokemon = battleFaintedEvent.killed
+        val victimEntity = victimPokemon.entity ?: return
+        val victimOwner = victimEntity.owner ?: return
+        if (victimPokemon.actor.type != ActorType.PLAYER) return
+
+        // Fetch killer
+        val killerEntity = victimPokemon.facedOpponents.first().entity;
+
+
+        if (victimPokemon.actor.pokemonList.all { it.health == 0 }) {
+            // player kill
+            if (killerEntity?.owner != null){
+                val killerOwner = killerEntity.owner as ServerPlayerEntity
+                victimOwner.damage(
+                    victimOwner.world.damageSources.playerAttack(killerOwner),
+                    Float.MAX_VALUE
+                )
+            }
+            // wild kill
+            else{
+                victimOwner.kill()
+            }
+
         }
     }
 }
